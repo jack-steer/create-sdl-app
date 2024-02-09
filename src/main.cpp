653b30655cbe7829896ed3a452ad45a18086d673
Jack Steer <jacksteer@jacks-mbp.home>
2024-02-09 15:37:27 +0000

    UPDATED readme and added io prompts

--- src/main.cpp
+++ src/main.cpp
@@ -37,17 +37,34 @@ bool containsLib(string dirname) {
 }
 
 int main(int argc, const char * argv[]) {
-    cout << "`Welcome to Create SDL App`!\n";
+    cout << "`Welcome to Create SDL App`!\n" << "\n\n\n";
+    
+    cout << "Checking if SDL is installed via brew..." << endl;
     bool libExists = containsLib("/usr/local/Cellar/sdl2/2.28.5/lib");
     if (libExists) {
         cout << "We've found a version of SDL" << endl;
     }
-    // If it already exists great
     
-    // Create new project, ask user for a name and directory
+    string projectPath;
+    string projectName;
+    
+    cout << "Please enter the path where your new project will be generated: " << endl;
+    cin >> projectPath;
+    
+    // Check project path is valid
+    
+    cout << "Please enter a name for your project: " << endl;
+    cin >> projectName;
     
     // Generate the new project with a blank window
     
     // Provide commands to build and run the new app
+    
+    cout << "Your new C++ SDL project has been generated!" << endl;
+    cout << "To run this project, run the following commands: " << endl;
+    
+    cout << "cd " << projectPath << "/" << projectName << endl;
+    cout << "clang++ main.cpp ./glad/src/glad.c -I/Library/Frameworks/SDL2.framework/Headers -I./glad/include -F/Library/Frameworks -framework SDL2 -rpath /Library/Frameworks";
+    cout << "./a.out" << endl;
     return 0;
 }
